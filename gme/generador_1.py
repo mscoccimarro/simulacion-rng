8a2ec012d78681e189ebd82d31fc2888de9cf3a2
mscoccimarro <maximiliano.scoccimarro@gmail.com>
2018-11-30 21:30:24 -0300

    Adds bar plots and improves lehmer generator function.

--- gme/generador_1.py
+++ gme/generador_1.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def showHeader(str):
   print('----------------------------------------------')
   print(str)
@@ -7,16 +9,39 @@ def showEnd():
   print('----------------------------------------------')
 
 def lehmerGenerator(a, m, seed, times):
-  x = seed
+  sequence = [seed]
   for i in range(0, times):
-    print(x, end="", flush=True)
-    print(',', end="", flush=True) if i < times-1 else print('...')
-    x = a*x % m
+    sequence.append(sequence[i]*a % m)
+  return sequence
 
+def stringifySequence(numberSequence):
+  return ', '.join(str(x) for x in numberSequence) + '...'
+
+sequence1 = lehmerGenerator(6, 13, 1, 13)
+sequence2 = lehmerGenerator(6, 13, 2, 13)
 
 showHeader('Generador de Lehmer: a = 6, m = 13, x1 = 1')
-lehmerGenerator(6, 13, 1, 13)
+print(stringifySequence(sequence1))
 
 showHeader('Generador de Lehmer: a = 6, m = 13, x1 = 2')
-lehmerGenerator(6, 13, 2, 13)
+print(stringifySequence(sequence2))
 showEnd()
+
+seq1 = lehmerGenerator(6, 13, 1, 20)
+seq2 = lehmerGenerator(6, 13, 2, 20)
+
+plt.title('Generador de Lehmer: a = 6, m = 13, x1 = 1')
+plt.xlabel('Índice en la secuencia')
+plt.ylabel('Número generado')
+seqBars = plt.bar(range(1, len(seq1) + 1), seq1)
+for bar in seqBars[12:]:
+  bar.set_color('green')
+plt.show()
+
+plt.title('Generador de Lehmer: a = 6, m = 13, x1 = 2')
+plt.xlabel('Índice en la secuencia')
+plt.ylabel('Número generado')
+seqBars = plt.bar(range(1, len(seq2) + 1), seq2)
+for bar in seqBars[12:]:
+  bar.set_color('green')
+plt.show()
